41de39ef91eebbc60b1cfd46ca96812049bb7f80
Nehal Sahu <67728142+NehalSahu8055@users.noreply.github.com>
2023-05-18 02:54:23 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,9 +22,37 @@ const generateAdvice = async () => {
 
 fetchBtn.addEventListener("click", generateAdvice);
 
-window.setTimeout( function() {
-  window.location.reload();
-}, 10000);
+const API_LINK = "https://api.adviceslip.com/advice",
+  adviceNumber = document.querySelector(".advice-gen__number"),
+  adviceQuote = document.querySelector(".advice-gen__quote"),
+  adviceOutput = document.querySelector(".advice-gen__output"),
+  fetchBtn = document.querySelector(".advice-gen__btn");
+
+  
+const fetchNewAdvice = async () => {
+  const response = await fetch(API_LINK);
+  const data = await response.json();
+  return data;
+};
+const renderAdvice = async (adviceData) => {
+  const { id, advice } = adviceData;
+  adviceNumber.textContent = `ADVICE #${id}`;
+  adviceQuote.textContent = advice;
+};
+const generateAdvice = async () => {
+  const data = await fetchNewAdvice();
+  const adviceData = data.slip;
+  renderAdvice(adviceData);
+};
+
+fetchBtn.addEventListener("click", generateAdvice);
+
+setInterval(generateAdvice, 10000);
+
+window.onload = () => {
+  generateAdvice();
+  
+};
 
 
 window.onload = () => {
