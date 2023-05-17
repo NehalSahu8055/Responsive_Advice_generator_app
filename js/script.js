3d7c5dd6d0ef17e38d236d6b18ab7812ceac98c0
Nehal Sahu <67728142+NehalSahu8055@users.noreply.github.com>
2023-05-18 00:20:16 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const API_LINK = "https://api.adviceslip.com/advice",
+  adviceNumber = document.querySelector(".advice-gen__number"),
+  adviceQuote = document.querySelector(".advice-gen__quote"),
+  fetchBtn = document.querySelector(".advice-gen__btn");
+
+const fetchNewAdvice = async () => {
+  const response = await fetch(API_LINK);
+  const data = await response.json();
+  return data;
+};
+const renderAdvice = async (adviceData) => {
+  const { id, advice } = adviceData;
+  adviceNumber.textContent = `ADVICE #${id}`;
+  adviceQuote.textContent = advice;
+};
+const generateAdvice = async () => {
+  const data = await fetchNewAdvice();
+  const adviceData = data.slip;
+  renderAdvice(adviceData);
+};
+
+fetchBtn.addEventListener("click", generateAdvice);
+
+window.onload = () => {
+  generateAdvice();
+};
