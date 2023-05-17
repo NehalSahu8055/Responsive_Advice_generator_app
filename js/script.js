a448c95f245c0d5f9a781dd1408519ceae5b8b47
Nehal Sahu <67728142+NehalSahu8055@users.noreply.github.com>
2023-05-18 03:01:00 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,6 @@
 const API_LINK = "https://api.adviceslip.com/advice",
   adviceNumber = document.querySelector(".advice-gen__number"),
   adviceQuote = document.querySelector(".advice-gen__quote"),
-  fetchBtn = document.querySelector(".advice-gen__btn");
-
-  
-const fetchNewAdvice = async () => {
-  const response = await fetch(API_LINK);
-  const data = await response.json();
-  return data;
-};
-const renderAdvice = async (adviceData) => {
-  const { id, advice } = adviceData;
-  adviceNumber.textContent = `ADVICE #${id}`;
-  adviceQuote.textContent = advice;
-};
-const generateAdvice = async () => {
-  const data = await fetchNewAdvice();
-  const adviceData = data.slip;
-  renderAdvice(adviceData);
-};
-
-fetchBtn.addEventListener("click", generateAdvice);
-
-const API_LINK = "https://api.adviceslip.com/advice",
-  adviceNumber = document.querySelector(".advice-gen__number"),
-  adviceQuote = document.querySelector(".advice-gen__quote"),
   adviceOutput = document.querySelector(".advice-gen__output"),
   fetchBtn = document.querySelector(".advice-gen__btn");
 
@@ -48,12 +24,6 @@ const generateAdvice = async () => {
 fetchBtn.addEventListener("click", generateAdvice);
 
 setInterval(generateAdvice, 10000);
-
-window.onload = () => {
-  generateAdvice();
-  
-};
-
 
 window.onload = () => {
   generateAdvice();
